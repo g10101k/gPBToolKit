76f7be42df680183cc1a4e3263572a9daf0c7d4e
Sergey V. Zhdanovskih <serg.zhdanovskih@yandex.ru>
2018-01-17 16:33:53 +0300

    Install scripts

---
+++ gPBToolKit/register_pbtk.cmd
@@ -0,0 +1,6 @@
+@echo off
+C:\windows\microsoft.net\Framework\v2.0.50727\Regasm.exe gPBToolKit.dll /codebase /tlb
+rem C:\windows\microsoft.net\Framework\v4.0.30319\Regasm.exe gPBToolKit.dll /unregister
+rem "C:\windows\microsoft.net\Framework64\v4.0.30319\Regasm.exe" gPBToolKit.dll /codebase /tlb
+pause
+
