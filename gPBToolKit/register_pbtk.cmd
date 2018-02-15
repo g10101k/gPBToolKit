f1ce62730135356e8883c92b01099e4a3fb0cdc0
Sergey V. Zhdanovskih <serg.zhdanovskih@yandex.ru>
2018-02-15 16:52:50 +0300

    New standalone application for work on ProcessBook displays

--- gPBToolKit/register_pbtk.cmd
+++ gPBToolKit/register_pbtk.cmd
@@ -1,6 +1,6 @@
 @echo off
-C:\windows\microsoft.net\Framework\v2.0.50727\Regasm.exe gPBToolKit.dll /codebase /tlb
-rem C:\windows\microsoft.net\Framework\v4.0.30319\Regasm.exe gPBToolKit.dll /unregister
-rem "C:\windows\microsoft.net\Framework64\v4.0.30319\Regasm.exe" gPBToolKit.dll /codebase /tlb
+C:\windows\microsoft.net\Framework\v2.0.50727\Regasm.exe gPBToolKit.dll /tlb
+rem C:\windows\microsoft.net\Framework\v4.0.30319\Regasm.exe gPBToolKit.dll /tlb
+rem "C:\windows\microsoft.net\Framework64\v4.0.30319\Regasm.exe" gPBToolKit.dll /tlb
 pause
 
